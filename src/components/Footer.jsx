16d9d23ccd7bd1b484bac8d9e3710db016e69b83
PatrickHabchi <habchipatrick@gmail.com>
2024-01-21 02:34:09 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
+import { Footer } from 'flowbite-react';
+import './Footer.css'*/
+
+
+export const Footer = () => {
+  return (
+    <div></div>
+  );
+};
